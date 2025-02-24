c32ea077b0df9d8a2bc9015ab0e64f1e79b16ce3
ttetricas <estermeireles89@gmail.com>
2025-02-23 23:37:40 -0300

    Consegui ufa

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+
+        Console.WriteLine("\nLove Me Harder\n");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(@"
+        Tell me something I need to know
+        Then take my breath and never let it go
+        If you just let me invade your space
+        I'll take the pleasure, take it with the pain
+        ");
+
+        Console.WriteLine("\n");
+        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine(@"
+        And if in the moment I bite my lip
+        Baby, in that moment you'll know this is
+        Something bigger than us and beyond bliss
+        Give me a reason to believe it
+        ");
+        Console.ResetColor();
+        
+        Console.WriteLine("\n");
+        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(@"
+        'Cause if you want to keep me, you gotta, gotta, gotta, gotta
+        Got to love me harder
+        And if you really need me, you gotta, gotta, gotta, gotta
+        Got to love me harder
+        ");
+        Console.ResetColor();
+
+        Console.WriteLine("\n");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(@"
+        (I would love you harder, love you, love you, love you...)
+        ");
+        Console.ResetColor();
